5b8a65de9effe4bc66a9f8540f4f506244bf5b59
ousuga <ousuga@gmail.com>
2023-05-24 19:12:43 -0500

    Se adicionaron gráficos y resumenes

--- Script.R
+++ Script.R
@@ -1,1 +1,10 @@
-print ("Este es el primer proyecto")
+
+# Resumen estadístico de los datos
+summary(salarios)
+
+# Análisis bivariados salarios vs categorias
+library(ggplot2)
+ggplot(salarios, aes(x = salary_in_usd, group = work_year, fill = work_year, 
+                     col = work_year)) +
+  geom_density(alpha = 0.4)
+
